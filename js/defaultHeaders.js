58fb915b506825d552eecf910b487b8a2171cb61
Ryan Hamblin <rhamblin.contractor@lifevantage.com>
2015-05-27 18:31:04 -0600

    first commit

---
+++ js/defaultHeaders.js
@@ -0,0 +1,10 @@
+var app = angular.module('parseQ');
+
+app.factory('httpRequestInterceptor', function () {
+	return {
+		request: function (config) {
+			config.headers = {'X-Parse-Application-Id': 'lp4Wrpx1dHA8JexYVrfx0Ys3YqJ1bTb3NiSGcebE', 'XqEtr3CbsHkrFiDKr0gbaKj53YiDjlZ0bUvxyrT5'};
+			return config;
+		}
+	};
+});
